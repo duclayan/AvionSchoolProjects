0859737aa4164a44739e71aab2021518879f346d
ykky <duclayan.dominique@gmail.com>
2020-09-09 18:01:16 +0400

    added functions according to tic tac toe rules

--- project-2-tictactoe/tic-tac-toe.js
+++ project-2-tictactoe/tic-tac-toe.js
@@ -6,9 +6,66 @@ const cellDivs = document.querySelectorAll('.game-cell');
 console.log(cellDivs);
 console.log(statusDiv);
 
+// game constants
+const xSymbol = '×';
+const oSymbol = '○';
 // game variables
 let gameIsLive = true; 
 let xIsNext = true; 
+let winner = null;
+
+//functions
+const letterSymbol = (letter) => letter === 'x' ? xSymbol : oSymbol;
+const handleWin = (letter) => {
+    gameIsLive = false;
+    winner = letter
+    if (winner === 'x') {
+        statusDiv.innerHTML = `${letterSymbol(winner)} has won!`;
+    } else {
+        statusDiv.innerHTML= `<span> ${letterSymbol(winner)} has won </span>`
+    }
+}
+const checkGameStatus = () => {
+    const topLeft = cellDivs[0].classList[2];
+    const topMiddle = cellDivs[1].classList[2];
+    const topRight = cellDivs[2].classList[2];
+    const middleLeft = cellDivs[3].classList[2];
+    const middleMiddle = cellDivs[4].classList[2];
+    const middleRight = cellDivs[5].classList[2];
+    const bottomLeft = cellDivs[6].classList[2];
+    const bottomMiddle = cellDivs[7].classList[2];
+    const bottomRight = cellDivs[7].classList[2];
+
+    //check winner
+    if(topLeft && topLeft === topMiddle && topLeft === topRight || ){
+        handleWin(topLeft);
+    } else if (middleLeft && middleLeft === middleMiddle && middleLeft === middleRight){
+        handleWin(middleLeft);
+    } else if (bottomLeft && bottomLeft === bottomMiddle && bottomLeft === bottomRight){
+        handleWin(bottomLeft);
+    } else if (topLeft && topleft === middleLeft && topLeft ===bottomLeft) {
+        handleWin(bottomLeft)
+    } else if (topMiddle && topMiddle === middleMiddle && topMiddle === bottomMiddle){
+        handleWin(topMiddle)
+    } else if (topRight && topRight === middleRight && topRight === bottomRight){
+        handleWin(topRight)
+    } else if (middleMiddle && middleMiddle === topRight && middleMiddle === bottomLeft){
+        handleWin(middleMiddle)
+    } else if (middleMiddle && middleMiddle === topLeft && middleMiddle === bottomRight){
+        handleWin(middleMiddle)
+    } else if (topLeft && topMiddle && topRight && middleRight && middleMiddle && middleLeft && bottomRight && bottomLeft && bottomMiddle){
+        gameIsLive = false;
+        statusDiv.innerHTML = "Game is Tied"
+    } else {
+        gameIsLive = true; 
+        xIsnext= !xIsNext;
+        if ( xIsNext) {
+            statusDiv.innerHTML = `${xSymbol} is next`
+        } else {
+            statusDiv.innerHTML = `<span> ${oSymbol} is next`
+        }
+    }
+};
 
 //event Handlers
 const handleReset = (e) => {
@@ -23,6 +80,7 @@ const handleCellClick = (e) => {
     }
     if(xIsNext) {
         classList.add('x')
+        checkGameStatus()
         xIsNext = !xIsNext;
     } else {
         classList.add('o')
