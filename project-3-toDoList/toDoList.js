01c80b3309dd1dc1f2aa4bef1034baa33cc6e2a5
ykky <duclayan.dominique@gmail.com>
2020-09-11 20:38:16 +0400

    add submit button to push new task

--- project-3-toDoList/toDoList.js
+++ project-3-toDoList/toDoList.js
@@ -1,7 +1,15 @@
-<<<<<<< HEAD
+//functions
+function addActivity(){
+    var newActivity = document.getElementById('activity-added').value
+    var node = document.createElement("ul")
+    var textNode = document.createTextNode(' ❌ ' + newActivity)
+    node.appendChild(textNode)
+    console.log(node)
+    document.getElementById('activity-list').appendChild(node)
+    newActivity = ' '
+}
 
-=======
->>>>>>> refs/remotes/origin/master
+
 let container = document.querySelector('.container')
 let header = container.querySelector('.header')
 let list = container.querySelector('.to-do-list')
@@ -10,3 +18,16 @@ let currentDate = header.querySelector('.currentDate')
 console.log(currentDate)
 let day = new Date()
 currentDate.innerHTML = day.constructor();
+
+//input the input
+let listContainer = document.querySelector('.to-do-list')
+let inputContainer = listContainer.querySelector('.input-field')
+let inputTask = inputContainer.querySelector('.new-item').value
+let add = inputContainer.querySelector('input-button')
+let keys = document.querySelector('.activity-list')
+
+keys.addEventListener('click',e => {
+    if(e.target.matches('ul')){
+        console.log('ul clicked')
+    }
+})
